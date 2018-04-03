5fae8acf39b590d23ea1a2a27e431c53bb90665e
Jerrod Mathis <jerrodmathis95@gmail.com>
2018-04-03 14:40:54 -0400

    Added Google Maps to landing page and app

--- src/pages/my-paved-way/my-paved-way.ts
+++ src/pages/my-paved-way/my-paved-way.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams, PopoverController } from 'ionic-angular';
+import { } from '@types/googlemaps';
 
 //import { CoursesPage } from '../courses/courses';
 
@@ -15,6 +16,18 @@ import { NavController, NavParams, PopoverController } from 'ionic-angular';
   templateUrl: 'my-paved-way.html',
 })
 export class MyPavedWayPage {
+
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 
   semester: string = "fall";
   coursesFall = [
